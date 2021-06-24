e3d4e0352e9bb147c4cd672a2d4bbf7b812764ee
Andrej Georgievski <andrej.georgievski@tomtom.com>
2021-06-24 05:25:01 +0200

    Improved requirements.txt, adding missing dependencies. Refactored setup script. Setup now considerably easier than before

--- setup.sh
+++ setup.sh
@@ -1,39 +1,42 @@
 #!/bin/bash
-# EXPERIMENTAL STUFF, NOT NECESSARY! install CUDART 11.2 libraries
-# sudo apt install cuda-cudart-11-2 cuda-cudart-dev-11-2
-# if errors are encountered:
-# sudo apt-get -o Dpkg::Options::="--force-overwrite" install --fix-broken
+BTS_PARENT_DIR=$(dirname "${PWD}")
+BTS_CODE_DIR="${PWD}"
+WORKSPACE_DIR="${BTS_PARENT_DIR}/bts_workspace"
+NEW_BTS_CODE_DIR="${WORKSPACE_DIR}/code"
 
 # Setup directories
-cd ..
-mkdir -p bts-workspace/dataset bts-workspace/
-mv bts bts-workspace/code
+cd ${BTS_PARENT_DIR}
+mkdir -p "${WORKSPACE_DIR}/dataset"
+mv "${BTS_CODE_DIR}" "${NEW_BTS_CODE_DIR}"
+BTS_CODE_DIR="${NEW_BTS_CODE_DIR}"
+unset NEW_BTS_CODE_DIR
 
-# Setup environment
-cd bts-workspace/code
-python3 -m virtualenv venv-bts
-source venv-bts/bin/activate
-PYTHONPATH=""
+# Setup virtual environment and dependencies
+cd "${BTS_CODE_DIR}"
 python3 -m pip install --upgrade pip
-python3 -m pip install tensorflow-gpu==1.13.1
-python3 -m pip install -r tensorflow/requirements.txt
-python3 -m pip install scikit-image
-python3 -m pip install PySide2==5.12
-python3 -m pip install pyopengl
-python3 -m pip install pyglm
+python3 -m pip install virtualenv
 
+python3 -m virtualenv venv-bts
+source "${BTS_CODE_DIR}/venv-bts/bin/activate"
+PYTHONPATH=""
+python3 -m pip install -r "${BTS_CODE_DIR}/tensorflow/requirements.txt"
 
-# Download model
-mkdir -p tensorflow/models
-python3 utils/download_from_gdrive.py 1nhukEgl3YdTBKVzcjxUp6ZFMsKKM3xfg tensorflow/models/bts_eigen_v2.zip
-cd tensorflow/models && unzip bts_eigen_v2.zip
+# Download model from Google Drive
+TF_MODELS_DIR="${BTS_CODE_DIR}/tensorflow/models"
+mkdir -p "${TF_MODELS_DIR}"
+python3 "${BTS_CODE_DIR}/utils/download_from_gdrive.py" \
+  1nhukEgl3YdTBKVzcjxUp6ZFMsKKM3xfg "${TF_MODELS_DIR}/bts_eigen_v2.zip"
+cd "${TF_MODELS_DIR}" && unzip bts_eigen_v2.zip
 
 # Setup build
-cd ../tensorflow/custom_layer/
-mkdir build && cd build
+TF_CUSTOM_LAYER_DIR="${BTS_CODE_DIR}/tensorflow/custom_layer"
+cd "${TF_CUSTOM_LAYER_DIR}"
+BTS_BUILD_DIR="${TF_CUSTOM_LAYER_DIR}/build"
+mkdir -p "${BTS_BUILD_DIR}" && cd "${BTS_BUILD_DIR}"
 # The code needs CUDA 10.0 (tested by the authors).
 # To specify which cuda version you want to use with CMake, use 
 # -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-10.0
 cmake -D CUDA_TOOLKIT_ROOT_DIR=/usr/local/cuda-10.0 ..
 make -j
 
+cd "${BTS_CODE_DIR}"
